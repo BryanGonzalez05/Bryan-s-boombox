d73209019d1a5af881328a91ccd21626f3d1753b
Bryan Gonzalez <Bryangonzalez9396@gmail.com>
2026-08-20 18:13:11 -0400

    created upload song post api route for backend

--- backend/controller/musicController.js
+++ backend/controller/musicController.js
@@ -1,42 +1,53 @@
-//create db connection and store filepath to db
 import db from '../db.js';
-
+import {parseFile} from 'music-metadata';
 
 export const UploadSong = async(req,res)=>{
-     
-   const files = req.files;
+   try{
+       const files = req.files;
+       const default_IMG_path = '/SongFolder/SongImagePlaceHolder.webp';
+       //checks if are files
+       if(!files ||files.length === 0){
+            return res.status(400).json({message: 'Error! there are no files inputted'})
+       }
+
+       //gets extra info from frontend
+       //you cant send json so backend because of middleware 
+       //it expects audio files with fieldname: files
+       //so send json as a string and its parse here. 
+       const songInfo = JSON.parse(req.body.songInfo);
+
+       //iterates through json
+       for(const si of songInfo){ 
+            //finds a match 
+            const match = files.find(f =>{
+                 return f.originalname === si.fileName;
+            })
+
+            if(!match){
+                 return res.status(400).json({message: `Error! file name ${si.fileName} does not match as listed`})
+            }
+
+            //store info to database
+            console.log(`filename : ${match.originalname}\n artist name: ${si.artistName}\n filepath: ${match.path}`)
+            const metadata = await parseFile(match.path);
+            await db.execute(
+                 `INSERT INTO songLib (songName, artistName, duration, songPath, imagePath) VALUES(?,?,?,?,?)`,
+                 [si.songName, si.artistName, metadata.format.duration, match.path, default_IMG_path]
+               );
+          
+       }
+
+       files.forEach(file => {
+            console.log(file);
+       });
+
+       return res.status(200).json({message: "Success"})
+   }
+   catch(error){
+     console.log(error);
+     return res.status(500).json({message: "internal server error"});
+   }
    
-   //checks if are files
-   if(!files ||files.length === 0){
-        return res.status(404).json({message: 'Error! there are no files inputted'})
-   }
-
-   //gets extra info from frontend
-   //you cant send json so backend because of middleware 
-   //it expects audio files with fieldname: files
-   //so send json as a string and its parse here. 
-   const songInfo = JSON.parse(req.body.songInfo);
-
-   //iterates through json
-   for(const si of songInfo){ 
-     //finds a match 
-     const match = files.find(f =>{
-          return f.originalname === si.fileName;
-     })
-
-     if(!match){
-          return res.status(400).json({message: `Error! file name ${si.fileName} does not match as listed`})
-     }
-
-     //store info to database
-     console.log(`filename : ${match.originalname}\n artist name: ${si.artistName}`)
-
-     
-   }
-
-   files.forEach(file => {
-        console.log(file);
-   });
-
-   return res.status(200).json({message: "Success"})
 }
+
+
