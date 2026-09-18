b0ccdfa8a24c301a6c1df7a9a37e67e0bf34d63c
Bryan Gonzalez <Bryangonzalez9396@gmail.com>
2026-09-18 15:51:42 -0400

    fully styled song and playlist elements and merged into one file

---
+++ frontend/homepage/homepageScript.js
@@ -0,0 +1,11 @@
+
+const songPageBTN = document.getElementById('songPageBTN');
+const playlistPageBTN = document.getElementById('playlistPageBTN');
+
+if(songPageBTN.classList.contains('active')) {
+    console.log('song page btn');
+}
+else{
+    console.log('playlist page btn');
+}
+
